e2e9e457c2525330645566e78ef3b6e57cc61467
PaulSohn <paulsohn1998@naver.com>
2015-05-15 21:05:10 +0900

    테스트 커밋
    Unity3D 프로젝트 파일을 전부 옮겨옴.

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Vector3 displacement;
+
+	private Transform player;
+	
+	void Awake () {
+		displacement=new Vector3(0,0,0);
+		player = GameObject.FindGameObjectWithTag("Player").transform;;
+	}
+
+	void FixedUpdate () {
+		transform.position=player.position+displacement;
+	}
+}
